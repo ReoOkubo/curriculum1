8e9e40f0a4f527425665c138dfaca1b324b1e309
ReoOkubo <appleanswer3@gmail.com>
2020-11-22 21:43:39 +0900

    課題の提出
    2-12

---
+++ 2-12/result2-12.php
@@ -0,0 +1,11 @@
+<?php
+$my_name = $_POST['my_name'];
+$prize = $_POST['prize'];
+$number = $_POST['number'];
+?>
+
+<p>お名前：<?php echo $my_name ?></p>
+<p></p>
+<p>ご希望商品<?php echo $prize ?></p>
+<p></p>
+<p>個数：<?php echo $number ?></p>
