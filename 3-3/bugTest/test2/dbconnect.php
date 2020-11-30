7afea6186f48bd3719a5314319c8f50b570d9baf
ReoOkubo <appleanswer3@gmail.com>
2020-11-30 18:08:52 +0900

    課題の提出
    3-3

---
+++ 3-3/bugTest/test2/dbconnect.php
@@ -0,0 +1,21 @@
+<?php
+// セッション開始
+// session_start();
+// DBサーバのURL
+$db['host'] = "localhost";
+// ユーザー名
+$db['user'] = "root";
+// ユーザー名のパスワード
+$db['pass'] = "root";
+// データベース名
+$db['dbname'] = "yigroupBlog";
+
+
+// define('DB_DATABASE', 'yigroupBlog');
+// // MySQLのユーザー名
+// define('DB_USERNAME', 'root');
+// // MySQLのログインパスワード
+// define('DB_PASSWORD', 'root');
+// // DSN
+// define('PDO_DSN', 'mysql:host=localhost;charset=utf8;dbname='.DB_DATABASE);
+
